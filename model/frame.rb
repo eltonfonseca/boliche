bb8bea2179ad8994a3bb4dd1f84d5331bf6e3e09
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-04-19 16:15:39 -0300

    #3 - Classe Frame implementada
    Classe Frame implementada, futuramente pode ocorrer modificações nesta classe.

---
+++ model/frame.rb
@@ -0,0 +1,36 @@
+# Classe responsavel por cada frame do jogo
+# @author Elton Fonseca
+class Frame 
+
+    # Responsavel por armazenar o valor da pontuação total
+    # @return [Fixnum] retorna o total do Score
+    attr_reader :score
+
+    # Construtor
+    # @param jogadas [Array<Jogada>] Jogadas que o jogador fez
+    def initialize(jogadas)
+        @jogadas = jogadas
+    end
+
+    # Pergunta se houve um Strike
+    # @return [true, false] retorna true ou false se houve ou não um Strike
+    def strike?
+        @jogadas[0] == 10
+    end
+
+    # Pergunta se houve um Spare
+    # @return [true, false] retorna true ou false se houve ou não um Spare
+    def spare?
+        if !strike? && @score == 10
+            true
+        else
+            false
+        end
+    end
+
+    # Calcula o Score total das jogadas
+    # @return [Fixnum] retorna o total de jogadas feitas
+    def score
+        @score = @jogadas[0] + @jogadas[1]
+    end
+end
