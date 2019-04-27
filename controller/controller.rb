a06df4f604852a6bb685b9eab82f26d5625b46e6
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-04-27 09:31:50 -0300

    Cria classe controladora
    Criação da classe controller para fazer a ponte entre os modelos e a visão.

---
+++ controller/controller.rb
@@ -0,0 +1,9 @@
+# Classe controladora, responsavel pelo intermédio entre a visão e os modelos
+# @author Elton Fonseca
+class Controller
+
+    # Construtor da Classe
+    def initialize
+
+    end
+end
