900d1d5cf1f6cdd510cb3be06ad3615b9891562a
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-06-01 12:52:35 -0300

    #28 - Teste de verificação do usuário logado

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get '/'
+class HomeControllerTest < ActionController::TestCase
+  
+  test 'se o usuario tiver logado' do
+    sign_in users(:one)
+    get :index
     assert_response :success
+  end
+
+  test 'se o usuario não tiver logado' do 
+    get :index
+    assert_response :redirect
   end
 
 end
