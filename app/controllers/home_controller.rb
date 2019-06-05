c2e7d98c1241b36a527b542453a975d44d44c353
Elton <elton@webgoal.com.br>
2019-06-05 09:13:51 -0300

    Criar pagina de jogada quando clicado na partida

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,22 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_user, :set_match, :set_adversario
+  
   def index
+    
+  end
+
+  private 
+
+  def set_match
+    @match = Match.find(params[:match_id])
+  end
+
+  def set_adversario
+    @adversario = Match.find(params[:match_id]).users.where(tipo_user: true)
+  end
+
+  def set_user
     @user = User.find(current_user)
-    p @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :matches
+  resources :matches do
+    resources :home
+  end
   devise_for :users
   root to: 'matches#index'
-  get 'home', to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
