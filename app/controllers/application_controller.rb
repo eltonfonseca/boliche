9a858ba4ca2bec70c78989e110d839a0e4038127
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-06-09 15:00:54 -0300

    Add estilização em tudo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nome, :email, :password, :idade) }    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
-
 end
