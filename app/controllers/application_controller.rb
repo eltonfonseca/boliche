2721b75d4bb898f8f62e11fd80afff6db81726b6
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-06-09 12:30:39 -0300

    Adicionado estilização com bootstrap e assets pipeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
- devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nome, :email, :password, :idade) }    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nome, :email, :password, :idade) }    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 
 end
